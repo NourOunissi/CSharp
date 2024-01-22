c2f9261eb4f5526258971ba7cab08a57847d005f
Nour Ounissi <ounissi.nour@outlook.fr>
2024-01-22 12:18:12 +0100

    TPCalc corrige

--- TPCalc/Calculatrice.cs
+++ TPCalc/Calculatrice.cs
@@ -5,44 +5,43 @@ namespace TPCalc
     public class Calculatrice
     {
         public decimal Addition(decimal a, decimal b)
-    {
-        return a + b;
-    }
-
-    public decimal Soustraction(decimal a, decimal b)
-    {
-        return a - b;
-    }
-
-    public decimal Multiplication(decimal a, decimal b)
-    {
-        return a * b;
-    }
-
-    public decimal Division(decimal a, decimal b)
-    {
-        if (b != 0)
         {
-            return (double)a / b;
+            return a + b;
         }
-        else
-        {
-            Console.WriteLine("Erreur : Division par zéro !");
-            return 0;
-        }
-    }    }
 
-    public decimal Modulo(decimal a, decimla b)
-    {
-        if (b != 0)
+        public decimal Soustraction(decimal a, decimal b)
         {
-            return a % b;
+            return a - b;
         }
-        else
+
+        public decimal Multiplication(decimal a, decimal b)
         {
-            Console.WriteLine("Erreur : Modulo par zéro !");
-            return 0;
+            return a * b;
+        }
+
+        public decimal Division(decimal a, decimal b)
+        {
+            if (b != 0)
+            {
+                return (decimal)a / b;
+            }
+            else
+            {
+                Console.WriteLine("Erreur : Division par zéro !");
+                return 0;
+            }
+        }
+        public decimal Modulo(decimal a, decimal b)
+        {
+            if (b != 0)
+            {
+                return a % b;
+            }
+            else
+            {
+                Console.WriteLine("Erreur : Modulo par zéro !");
+                return 0;
+            }
         }
     }
 }
-
