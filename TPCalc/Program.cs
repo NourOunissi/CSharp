69297e26e5c3fa72a51a8b44c544efaf07243146
Nour Ounissi <ounissi.nour@outlook.fr>
2024-01-22 12:10:09 +0100

    TPCalc

--- TPCalc/Program.cs
+++ TPCalc/Program.cs
@@ -6,3 +6,17 @@ Calculatrice maCalculatrice = new Calculatrice();
 
 decimal resultatAddition = maCalculatrice.Addition(10, 5);
 Console.WriteLine("Addition : " + resultatAddition);
+
+decimal resultatSoustraction = maCalculatrice.Soustraction(1, 52);
+Console.WriteLine("Soustraction : " + resultatSoustraction);
+
+decimal resultatMultiplication = maCalculatrice.Multiplication(5, 3);
+Console.WriteLine("Multiplication : " + resultatMultiplication);
+
+decimal resultatDivision = maCalculatrice.Division(13, 2);
+Console.WriteLine("Division : " + resultatDivision);
+
+decimal resultatModulo = maCalculatrice.Modulo(8, 9);
+Console.WriteLine("Modulo : " + resultatModulo);
+
+
